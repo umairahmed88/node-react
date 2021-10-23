4fd2ec4fd6b7ef944ecabfcf2920d94c517aa6f3
umairahmed88 <umairahmed_88@ymail.com>
2021-10-23 23:30:46 +0500

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send({ hi: "Umair" })
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
 
 // app.get => route handler
 //  / => access root route
@@ -14,5 +31,5 @@ app.get("/", (req, res) => {
 // app.listen(5000) => it tell the node that express want to listen on port 5000  
 // const PORT = process.env.PORT => when heroku run the app it has ability to inject environment variables, these variables are that are setup run time that node are runing on the top of, that some thing heroku want to tell us after we deploy our app
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
