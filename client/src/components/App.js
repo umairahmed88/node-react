cce060aea9283d32b6b0e83c7b1db1721b11923a
umairahmed88 <umairahmed_88@ymail.com>
2021-10-27 15:03:55 +0500

    changed google strategy settings

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import * as actions from "../actions"
 
 import Header from "./Header";
 import Landing from "./Landing";
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>Survey New</h2>;
 
