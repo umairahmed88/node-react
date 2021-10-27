cbb0b06232fdb72ff5a47647ba34c391ba8e235c
umairahmed88 <umairahmed_88@ymail.com>
2021-10-27 15:05:32 +0500

    changed google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
         {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "http://localhost:5000/auth/google/callback",
         proxy: true
         }, 
     async (accessToken, refreshToken, profile, done) => {
